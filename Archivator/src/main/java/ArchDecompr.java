b5aa4f1a3c0f6a50de227d175e2f5a71424e1043
Mr.Rustam <13rustam.1993@mail.ru>
2014-02-01 17:23:00 +0400

    Add new rename variables

---
+++ Archivator/src/main/java/ArchDecompr.java
@@ -0,0 +1,43 @@
+
+public class ArchDecompr {
+    /**
+     * Decompress method. if is found number - N, N is recorded after the number of characters
+     * @param DecomprText text which should be decompressed
+     * @return Decompressed text
+     */
+    public String Decompress(String DecomprText)
+    {
+        char ch;
+        boolean RepChar = false;
+        int NumRepeat=1;
+        String ComprText = "";
+        for (int i=0;i<DecomprText.length();i++)
+        {
+            ch = DecomprText.charAt(i);
+            if (ch > 47 && ch < 58 )
+            {
+                if (RepChar)
+                {
+                    NumRepeat = NumRepeat*10 + (int)(ch) - 48;
+                }
+                else
+                {
+                    NumRepeat = DecomprText.charAt(i) - 48;
+                    RepChar = true;
+                }
+            }
+            else
+            {
+                if (RepChar)
+                {
+                    for (int k = 0;k < NumRepeat;k++)
+                        ComprText = ComprText + ch;
+                    RepChar = false;
+                }
+                else
+                    ComprText = ComprText + ch;
+            }
+        }
+        return ComprText;
+    }
+}
